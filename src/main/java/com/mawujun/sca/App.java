689075d7001a2b8fa1ce6c9f52dda94e5cec2bc8
mawujun1234 <mawujun1234@163.com>
2020-01-11 20:54:32 +0800

    init

---
+++ src/main/java/com/mawujun/sca/App.java
@@ -0,0 +1,13 @@
+package com.mawujun.sca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
